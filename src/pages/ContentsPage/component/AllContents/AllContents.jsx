ee900d663afeb92a3090a28f52678ed179666632
leesangwon231 <werggf@naver.com>
2024-09-11 00:02:12 +0900

    first commit

--- src/pages/ContentsPage/component/AllContents/AllContents.jsx
+++ src/pages/ContentsPage/component/AllContents/AllContents.jsx
@@ -5,32 +5,33 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import {useContents} from "../../../../hooks/useContents.jsx";
 import ContentCard from "../ContentCard/ContentCard.jsx";
+import "./AllContents.css"
 
 const  AllContents = () => {
 
-/* 파라미터 (데이터 검색용)
-        * shprfnm : 연극명 (검색)
-        * signgucode : 지역 (
-        * 서울광역시 : 11  , 부산광역시 : 26 , 대구광역시 : 27,
-        * 인천광역시 : 28  , 광주광역시 : 29 , 대전광역시 : 30 ,
-        * 울산광역시 : 31 , 세종특별자치도 : 36 , 경기도 : 41,
-        * 강원특별자치도 : 51 , 충청북도 : 43 , 충청남도 : 44 ,
-        * 전북특별자치도 : 45 , 전라남도 : 46 , 경상북도 : 47,
-        * 경상남도 : 48 , 제주특별자치도 : 50
-        * )
-        * prfstate : 공연상태코드 (공연 예정 : 01, 공연중 : 02 , 공연완료 : 03)
-        * shcate : 장르 (GGGA : 뮤지컬 , CCCD : 콘서트 , BBBC : 클래식/무용 , AAAA :연극)
-        * */
+    /* 파라미터 (데이터 검색용)
+            * shprfnm : 연극명 (검색)
+            * signgucode : 지역 (
+            * 서울광역시 : 11  , 부산광역시 : 26 , 대구광역시 : 27,
+            * 인천광역시 : 28  , 광주광역시 : 29 , 대전광역시 : 30 ,
+            * 울산광역시 : 31 , 세종특별자치도 : 36 , 경기도 : 41,
+            * 강원특별자치도 : 51 , 충청북도 : 43 , 충청남도 : 44 ,
+            * 전북특별자치도 : 45 , 전라남도 : 46 , 경상북도 : 47,
+            * 경상남도 : 48 , 제주특별자치도 : 50
+            * )
+            * prfstate : 공연상태코드 (공연 예정 : 01, 공연중 : 02 , 공연완료 : 03)
+            * shcate : 장르 (GGGA : 뮤지컬 , CCCD : 콘서트 , BBBC : 클래식/무용 , AAAA :연극)
+            * */
 
 
     const [originData , setOriginData] = useState([]);
 
 
     const [param , setParam] = useState({    shprfnm : "",
-                                                            signgucode : "",
-                                                            prfstate : "",
-                                                            shcate : "",
-                                                        });
+        signgucode : "",
+        prfstate : "",
+        shcate : "",
+    });
 
     const {data} = useContents(param);
 
--- src/pages/ContentsPage/component/ContentCard/ContentCard.jsx
+++ src/pages/ContentsPage/component/ContentCard/ContentCard.jsx
@@ -5,15 +5,15 @@ const ContentCard = ({content}) => {
     const {data : detailData} = useContentsDetail(content?.mt20id);
     const detailContent = detailData?.dbs.db;
 
-  return (
-       <div className={"ContentsPage_Card_Container"}>
-           <img className={"ContentsPage_Card_Image"} src={detailContent?.poster} alt={""}></img>
-           <div className={"ContentsPage_Card_Info"}>
-               <p>{content?.prfnm}</p>
-               <p>{content?.fcltynm}</p>
-           </div>
-       </div>
-  );
+    return (
+        <div className={"ContentsPage_Card_Container"}>
+            <img className={"ContentsPage_Card_Image"} src={detailContent?.poster} alt={""}></img>
+            <div className={"ContentsPage_Card_Info"}>
+                <p className={"ContentsPage_Title"}>{content?.prfnm}</p>
+                <p className={"ContentsPage_Info"}>{content?.fcltynm}</p>
+            </div>
+        </div>
+    );
 }
 
 export default ContentCard;
