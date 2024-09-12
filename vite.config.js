6063a8be3dce07bb68fc13bc1434f29da9fa33ff
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-09-12 17:20:44 +0900

    api fixed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,8 +10,8 @@ export default defineConfig({
         target: 'http://www.kopis.or.kr/openApi/restful/',
         // target: 'http://www.kopis.or.kr/openApi/restful/pblprfr',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
