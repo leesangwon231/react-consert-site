3e6898340d9a8f7bcc0202425d0ff33487ba3773
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-09-13 11:23:47 +0900

    card design edited

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
         {array.map((i) => (
           <div key={i} className="card-box border p-3 shadow-sm rounded-3">
             <div className="img-box">
-              <img src="http://www.kopis.or.kr/upload/pfmPoster/PF_PF248732_240910_125126.png" alt="" />
+              <img src="http://www.kopis.or.kr/upload/pfmPoster/PF_PF248732_240910_125126.png" alt="" className='rounded-3'/>
             </div>
             <div className="text-box mt-3">
               <h5 className="fw-bold mb-3">ARTMS World Tour, Moonshot [서울]</h5>
