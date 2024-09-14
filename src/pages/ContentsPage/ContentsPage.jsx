b9a467b346137d8e0fdcdfcbe754cde303ce1412
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-09-14 16:42:18 +0900

    edit contents page

--- src/pages/ContentsPage/ContentsPage.jsx
+++ src/pages/ContentsPage/ContentsPage.jsx
@@ -1,67 +1,74 @@
-import React, { useEffect } from "react";
-import {Navigate, useNavigate} from "react-router-dom";  // useNavigate 추가
-import { useContents } from "../../hooks/useContents";
+import {Navigate} from 'react-router-dom'; // useNavigate 추가
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./ContentsPage.css"
-import AllContents from "./component/AllContents/AllContents.jsx";
-import MyLocationContents from "./component/MyLocationContents/MyLocationContents.jsx";
-import {useParams} from "react-router-dom";
-import Slider from "./component/common/Slider/Slider.jsx";
+import './ContentsPage.css';
+import AllContents from './component/AllContents/AllContents.jsx';
+import MyLocationContents from './component/MyLocationContents/MyLocationContents.jsx';
+import {useParams} from 'react-router-dom';
+import {useState} from 'react';
+
 const ContentsPage = () => {
-    let params = useParams();
+  const params = useParams();
 
+  const [performanceKinds, setPerformanceKinds] = useState(['콘서트', 'CCCD']);
 
-    let performanceKinds = ["콘서트", "CCCD"];
+  if (params.category === 'musical') {
+    setPerformanceKinds(['뮤지컬', 'GGGA']);
+  } else if (params.category === 'classical') {
+    setPerformanceKinds(['클래식/무용', 'BBBC']);
+  } else if (params.category === 'play') {
+    setPerformanceKinds(['연극', 'AAAA']);
+  } else if (params.category === 'concert') {
+    setPerformanceKinds(['콘서트', 'CCCD']);
+  } else {
+    setPerformanceKinds(['콘서트', 'CCCD']);
+    return <Navigate to="/contents/concert" />;
+  }
 
-    if(params.category === "musical"){
-        performanceKinds = ["뮤지컬", "GGGA"]
-    }else if(params.category === "classical"){
-        performanceKinds = ["클래식/무용" , "BBBC"]
-    }else if(params.category === "play"){
-        performanceKinds = ["연극", "AAAA"]
-    }else if(params.category === "concert"){
-        performanceKinds = ["콘서트", "CCCD"];
-    } else{
-        performanceKinds = ["콘서트", "CCCD"];
-        return <Navigate to ="/contents/concert" />
-    }
+  const ctprvn = {
+    서울특별시: 11,
+    부산광역시: 26,
+    대구광역시: 27,
+    인천광역시: 28,
+    광주광역시: 29,
+    대전광역시: 30,
+    울산광역시: 31,
+    세종특별자치시: 36,
+    경기도: 41,
+    강원특별자치도: 42,
+    충청북도: 43,
+    충청남도: 44,
+    전라북도: 45,
+    전라남도: 46,
+    경상북도: 47,
+    경상남도: 48,
+    제주특별자치도: 50,
+  };
 
-    const ctprvn = {
-        "서울특별시": 11,"부산광역시": 26,"대구광역시": 27,
-        "인천광역시": 28, "광주광역시": 29,"대전광역시": 30,
-        "울산광역시": 31,"세종특별자치시": 36,"경기도": 41,
-        "강원특별자치도": 42,"충청북도": 43,"충청남도": 44,
-        "전라북도": 45,"전라남도": 46,"경상북도": 47,
-        "경상남도": 48,"제주특별자치도": 50
-    };
+  const sortedState = {
+    가나다순: '01',
+    최신순: '02',
+  };
 
-    const sortedState = {
-        "가나다순": "01","최신순": "02"
-    }
+  const perFormanceState = {
+    공연예정: '01',
+    공연중: '02',
+    공연완료: '03',
+  };
 
-    const perFormanceState = {
-        "공연예정": "01","공연중": "02","공연완료": "03",
-    }
+  const performanceFilter = [ctprvn, sortedState, perFormanceState];
 
-    let performanceFilterArray = [ctprvn,sortedState,perFormanceState]
+  const performanceFilterArray = performanceFilter?.map((state) =>
+    Object.entries(state).map((data) => {
+      return {[data[1]]: data[0]};
+    })
+  );
 
-    performanceFilterArray = performanceFilterArray?.map((state)=>(
-        Object.entries(state)
-            .map((data) =>{
-                return { [data[1]] : data[0]}
-            })
-    ));
-
-
-
-
-
-    return (
-        <div className={"ContentsPage_Container"}>
-            <AllContents performanceFilterArray = {performanceFilterArray} performanceKinds={performanceKinds}/>
-            <MyLocationContents ctprvn ={ctprvn} performanceKinds = {performanceKinds}/>
-        </div>
-    );
+  return (
+    <div className={'ContentsPage_Container'}>
+      <AllContents performanceFilterArray={performanceFilterArray} performanceKinds={performanceKinds} />
+      <MyLocationContents ctprvn={ctprvn} performanceKinds={performanceKinds} />
+    </div>
+  );
 };
 
 export default ContentsPage;
