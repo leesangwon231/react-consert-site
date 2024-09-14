7567a61ca6a9e47803463a9f44afb5d68ff6b724
subeen <soobin2318@gmail.com>
2024-09-14 23:45:49 +0900

    hall navigation

--- src/pages/SearchPage/component/ListCenterContainer/ListCenterContainer.jsx
+++ src/pages/SearchPage/component/ListCenterContainer/ListCenterContainer.jsx
@@ -1,42 +1,48 @@
 import React from 'react';
 import { useSearchCenterDeatils } from '../../../../hooks/useSearchCenterDetail';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faUtensils, faMugSaucer, faStore, faWheelchair, faSquareParking  } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faUtensils, faMugSaucer, faStore, faWheelchair, faSquareParking } from '@fortawesome/free-solid-svg-icons';
 import "./ListCenterContainer.css";
+import { useNavigate } from 'react-router-dom';
 
-const ListCenterContainer = ({ data }) => { 
-    const { data: centerData, error, isLoading } = useSearchCenterDeatils(data); 
+const ListCenterContainer = ({ data }) => {
+    const { data: centerData, error, isLoading } = useSearchCenterDeatils(data.mt10id);
+    const navigate = useNavigate();
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
 
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
-
+    const centerEvents = centerData?.dbs?.db || {};
     const relateUrl = centerData?.dbs?.db?.relateurl?.trim();
-    const centerEvents = centerData?.dbs?.db || [];
     const hasWheelchair = 
         centerEvents.parkbarrier === 'Y' || 
         centerEvents.restbarrier === 'Y' || 
         centerEvents.runwbarrier === 'Y' || 
         centerEvents.elevbarrier === 'Y';
-
     const shouldHideDetail = 
         !centerEvents.fcltynm 
         && !centerEvents.adres 
         && !centerEvents.telno 
-        && 
-            !(centerEvents.restaurant !== 'N' || 
+        && !(centerEvents.restaurant !== 'N' || 
             centerEvents.cafe !== 'N' || 
             centerEvents.store !== 'N' || 
             hasWheelchair || 
             centerEvents.parkinglot !== 'N');
 
+    const handleClick = (id) => {
+        if (id) {
+            navigate(`/hall/${id}`);
+        } else {
+            console.error('ID is undefined');
+        }
+    };
+
     return (
         <div className='search-center-detail'>
-            <div className='search-center-container'>
+            <div className='search-center-container'
+                onClick={() => handleClick(data.mt10id)}
+                style={{ cursor: 'pointer' }}
+            >
                 <div className={`list-detail ${shouldHideDetail ? 'hidden' : ''}`}>
                     {centerEvents.fcltynm}
                     {relateUrl && (
