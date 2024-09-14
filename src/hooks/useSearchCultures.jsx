267704e26b7a7350cfd1585824ec3c786b31a5fd
subeen <soobin2318@gmail.com>
2024-09-15 04:40:47 +0900

    error hook update

--- src/hooks/useSearchCultures.jsx
+++ src/hooks/useSearchCultures.jsx
@@ -5,53 +5,38 @@ import api from "../utils/api.jsx";
 const parseXml = async (xml) => {
     const parser = new xml2js.Parser({ explicitArray: false });
     try {
-        const result = await parser.parseStringPromise(xml);
-        return result;
+        return await parser.parseStringPromise(xml);
     } catch (err) {
         throw new Error('XML 파싱 오류: ' + err.message);
     }
 };
 
-const fetchSearchContentsData = async (param) => {
-    const { shprfnm = '' } = param.queryKey[1] || {};
-    let allResults = [];
-    let cpage = 1;
-    let hasMoreData = true;
-
+const fetchSearchContentsData = async ({ queryKey }) => {
+    const { shprfnm = '' } = queryKey[1] || {};
     try {
-        while (hasMoreData) {
-            const response = await api.get('pblprfr', {
-                params: {
-                    shprfnm,
-                    stdate: '20240101',
-                    eddate: '20241231',
-                    cpage: cpage.toString(),
-                    rows: '5'
-                }
-            });
-
-            const xmlData = response.data;
-            const jsonData = await parseXml(xmlData);
-
-            if (jsonData.dbs && jsonData.dbs.db) {
-                const currentResults = jsonData.dbs.db;
-                if (Array.isArray(currentResults) && currentResults.length > 0) {
-                    allResults = [...allResults, ...currentResults];
-                } else {
-                    hasMoreData = false;
-                }
-            } else {
-                hasMoreData = false;
+        const response = await api.get('pblprfr', {
+            params: {
+                shprfnm,
+                stdate: '20240101',
+                eddate: '20241231',
+                cpage: '1', 
+                rows: '1000'
             }
+        });
 
-            cpage++;
+        const xmlData = response.data;
+        const jsonData = await parseXml(xmlData);
+
+        if (jsonData.dbs && jsonData.dbs.db) {
+            const currentResults = jsonData.dbs.db;
+            return { dbs: { db: Array.isArray(currentResults) ? currentResults : [] } };
+        } else {
+            return { dbs: { db: [] } }; 
         }
-
-        return { dbs: { db: allResults } };
 
     } catch (error) {
         console.error('데이터 가져오기 오류:', error);
-        throw error;
+        throw new Error('데이터 가져오기 오류: ' + error.message);
     }
 };
 
@@ -61,4 +46,4 @@ export const useSearchCultures = (param) => {
         queryFn: fetchSearchContentsData,
         retry: 1,
     });
-}
+};
