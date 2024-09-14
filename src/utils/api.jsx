24906579feb011d4e2622bd373afda61e3cc1b7f
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-09-14 18:03:39 +0900

    api fix try 8

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const API = window.location.hostname === 'localhost' ? '/api' : '/api';
+const API = window.location.hostname === 'localhost' ? '/api' : '/proxy';
 const api = axios.create({
   // baseURL: '/api',
   baseURL: API,
