387f8f9999788140cace0a3dbe03c7d4b0ac00af
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-09-14 19:07:02 +0900

    api error fix try 16

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const API_KEY = import.meta.env.VITE_API_KEY;
-const API = window.location.hostname === 'localhost' ? '/api' : '/proxy';
+
 const api = axios.create({
-  // baseURL: '/api',
-  baseURL: API,
+  baseURL: '/api',
   params: {
     service: API_KEY,
   },
