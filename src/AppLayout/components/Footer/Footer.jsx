0266deb7cb588a4257909f0665f0f58956b17a99
Sungmin-Kim-Dev <dreamerk24dev@gmail.com>
2024-09-15 18:54:16 +0900

    edit footer

--- src/AppLayout/components/Footer/Footer.jsx
+++ src/AppLayout/components/Footer/Footer.jsx
@@ -1,71 +1,73 @@
-import { Container, Row, Col } from "react-bootstrap";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
-import './FooterStyle.css'
+import {Container, Row, Col} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {faUser} from '@fortawesome/free-solid-svg-icons';
+import './FooterStyle.css';
 
 const Footer = () => {
   return (
-    <footer className="py-4 mt-5" style={{ borderTop: "2px solid #dfdfdfaa" }}>
+    <footer id="footer" className="py-4 mt-5" style={{borderTop: '2px solid #dfdfdfaa'}}>
       <Container>
         <Row className="mt-3">
-          <Col
-            md={5}
-            className="mb-3 text-center cus-line"
-            >
-            <h2>
-              <strong>Vite REACT Project</strong>
-            </h2>
-            <p>Project Duration</p>
-            <p>24.09.08 - 24.09.15</p>
+          <Col md={6} className="pb-3 text-center cus-line">
+            <h2 className="mb-2">코알누 리액트 그룹 프로젝트 8조</h2>
+            <h4 className="mb-4">공연 예술 정보 페이지</h4>
+            <p className="mb-2">Project Duration</p>
+            <p>2024.09.08 - 2024.09.15</p>
             <br />
-            <h5>GitHub</h5>
             <a
               href="https://github.com/leesangwon231/react-consert-site"
               target="_blank"
               rel="noopener noreferrer"
-              className="me-3 mb-4"
-            >
+              className="d-flex align-items-center gap-2 justify-content-center">
+              <h5>GitHub</h5>
               <FontAwesomeIcon icon={faGithub} size="2x" />
             </a>
           </Col>
 
-          <Col md={7} className="mb-3 text-center">
-            <Row>
-              <p className="mb-3">
+          <Col md={6} className="my-3">
+            <div className="d-flex flex-column justify-content-center">
+              <div className="mb-3 fs-4 d-flex gap-2 align-items-center justify-content-center">
                 <FontAwesomeIcon icon={faUser} /> Team Member
-              </p>
-              <div style={{ lineHeight: "1.5" }}>
-                <p><strong>김성민</strong> &nbsp;&nbsp;&nbsp;&nbsp; PO  |  Home Page</p>
-                <p><strong>이상원</strong> &nbsp;&nbsp;&nbsp;&nbsp; SM | Contents Page</p>
-                <p><strong>정민지</strong> &nbsp;&nbsp;&nbsp;&nbsp; Culture Page</p>
-                <p><strong>도수빈</strong> &nbsp;&nbsp;&nbsp;&nbsp; Search Page</p>
-                <p><strong>최지윤</strong> &nbsp;&nbsp;&nbsp;&nbsp; ContentsDeatil Page</p>
               </div>
-              
-            </Row>
+              <div className="name-list fs-5 d-flex align-self-center flex-column row-gap-2">
+                <div className="name-box">
+                  <span className="name me-3 fw-bold">김성민</span>
+                  <span>PO | 메인, 순위 페이지</span>
+                </div>
+                <div className="name-box">
+                  <span className="name me-3 fw-bold">이상원</span>
+                  <span>PO | 콘텐츠 페이지</span>
+                </div>
+                <div className="name-box">
+                  <span className="name me-3 fw-bold">정민지</span>
+                  <span>PO | 지역, 공연장 상세 페이지</span>
+                </div>
+                <div className="name-box">
+                  <span className="name me-3 fw-bold">도수빈</span>
+                  <span>PO | 검색 페이지</span>
+                </div>
+                <div className="name-box">
+                  <span className="name me-3 fw-bold">최지윤</span>
+                  <span>PO | 콘텐츠 상세 페이지</span>
+                </div>
+              </div>
+            </div>
           </Col>
         </Row>
-        <hr style={{ borderTop: "3px solid #dfdfdfaa" }} />
+        <hr style={{borderTop: '3px solid #dfdfdfaa'}} />
         <Row>
-          <Col className="text-center">
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <p className="me-4">API Sources</p>
-              <a
-                href="http://www.kopis.or.kr"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="me-4"
-              >
-                (재)예술경영지원센터 공연예술통합전산망
-              </a>
-              <a
-                href="https://developers.kakao.com/console/app"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                KAKAO developers
-              </a>
+          <Col>
+            <div className="d-flex justify-content-center flex-wrap gap-2">
+              <span>사용 API</span>
+              <div className="link-box">
+                <a href="http://www.kopis.or.kr" target="_blank" rel="noopener noreferrer" className='mb-1'>
+                  (재)예술경영지원센터 공연예술통합전산망
+                </a>
+                <a href="https://developers.kakao.com/console/app" target="_blank" rel="noopener noreferrer">
+                  KAKAO developers
+                </a>
+              </div>
             </div>
           </Col>
         </Row>
